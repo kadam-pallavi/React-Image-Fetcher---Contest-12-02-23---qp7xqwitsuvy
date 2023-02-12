c70ce947bbd13d274f636d0bd780a22e10ef5285
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-02-12 13:50:42 +0530

    Update PhotoFrame.js

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import React, { useState, useEffect } from 'react';
+import{ Loder }from './Loader';
 
-export const PhotoFrame = () => {
+
+
+export const PhotoFrame = (props) => {
+   const {title, url}=props;
    
+   return(
+      <div className='photoframe'>
+      {url && <img src={url}/>}
+{title && <div className='caption'>
+   {title}</div>}
+ </div>
+ );
 }
